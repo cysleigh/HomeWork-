556844f5253656425c4af65e6c44d1f0a8cdf137
sleigh <jj81927@gmail.com>
2019-08-08 23:55:42 +0800

    購物車上傳

--- cart.php
+++ cart.php
@@ -1,22 +1,11 @@
-<h2>購物車中有以下商品：</h2>
-<table cellpadding="0" cellspacing="0" border="1" width="100%">
-<tr>
-<td>商品名稱</td>
-<td>商品單價</td>
-<td>購買數量</td>
-<td></td>
-</tr>
+<form action="secret.php" method="POST">
 <?php
-session_start();
-//$uid=$_SESSION["uid"];
-$arr=array();
-if(!empty($_SESSION["gwc"]))
-{
-$arr=$_SESSION["gwc"];
-}
-require ("config.php");
+	session_start();			
+	$cart = $_SESSION['cart'];	
+	foreach($cart as $i=>$c){	
+		
+		echo "<input type='text' value='$i' name='d[]' />".$i.' 数量：'.$c."<br />";
+	}
 ?>
-</table>
-<div>
-<a href="index.php" rel="external nofollow" rel="external nofollow" >回首頁</a> 
-<a href="secret.php" rel="external nofollow" rel="external nofollow" >瀏覽商品</a> 
+	<input type="button" value="返回" οnclick="location='secret.php';" />
+</form>
--- shoppage.php
+++ shoppage.php
@@ -2,22 +2,20 @@
 session_start();
 header("content-type:text/html; charset=utf-8");
 //require_once("secret.php");
-$nb=$_GET["nb"]; //商品編號
-$cName=$_GET["cName"]; //商品名稱
-$arr=array();
-if(!empty($_SESSION["arr"]))
-{
-$arr = array(array($nb,1));
-$_SESSION["arr"] = $arr;
-
-//header("Location: secret.php");
-}else{
-    $arr = array(array($nb,1));
-    $_SESSION["arr"] = $arr;
-    //header("Location: secret.php"); //跳轉到選購介面(secret.php)
+// $nb=$_GET["nb"]; //商品編號
+// $cName=$_GET["cName"]; //商品名稱
+if(!isset($_SESSION['cart'])){	//查看是否有購物車session
+    $arr = $_SESSION['cart'];
+}
+if(isset($_GET['nb'])){		
+	 $arr = $arr + $_GET['nb'];
+    var_dump($_SESSION['cart']);
+    // foreach($arr as $key =>$value){
+    //     echo "$key -> $value";
+	//   }
 }
 
-echo var_dump($_SESSION["arr"];
 
-
+//header("Location: secret.php"); //跳轉到選購介面(secret.php)
 ?>
+<a href="secret.php">查看購物車</a><br/>
